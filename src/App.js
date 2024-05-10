631e71ce435258903eae9c0f446a7c0c1bb5ab92
Chibu <chibueze.k.muoneke@gmail.com>
2024-05-10 16:31:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from './components/Main'
 
-function App() {
+
+const initialState={
+  questions:[],
+
+  //Loading, error, ready, active, finished
+  status:'loading'
+}
+
+function reducer(state,action){
+  switch(action.type){
+    case 'dataReceived':
+      return{ 
+        ...state,
+        questions: action.payload,
+        status:'ready'
+      };
+    default:
+    throw new Error('Action unknown');
+  }
+}
+
+export default function App() {
+
+const [state, dispatch] = useReducer(reducer,initialState)
+
+
+  useEffect(function(){
+    fetch('http://localhost:3000/questions').then(
+      res=>res.json()
+    ).then(data=>dispatch({type:'dataReceived', payload:data})).catch(err=>console.error(err))
+  },[])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header/>
+     <Main> 
+       <p>1/15</p>
+       <p>Question</p>
+     </Main>
     </div>
   );
 }
-
-export default App;
